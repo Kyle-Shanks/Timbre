a7a6db1bcad9a424f0a8c33e1e874a6cb81af3db
KJ Shanks <kyle.j.shanks@gmail.com>
2021-02-06 14:21:55 -0500

    Initial setup

---
+++ src/Timbre.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(<div>Hello, world! This is Timbre.</div>, document.getElementById('root'));
+});
